00b312af05e41c8903c28369454833a8f53d8184
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2025-07-18 22:13:36 +0530

    Update stack_1.cpp

--- stack_1.cpp
+++ stack_1.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main ()
 {
-   push(20);
-   push(30);
-   push(40);
-   display();
+   push(20);  // pushes 20
+   push(30);  // pushes 30
+   push(40);  // pushes 40
+   display(); // display 40 30 20
 }
