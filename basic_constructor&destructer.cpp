d76dd421499e9fb2ef0c8c1c49c7e908da7c206c
Ravitejakolaparthi <ravitejakolaparthi97@gmail.com>
2025-07-21 08:47:22 +0530

    Update basic_constructor&destructer.cpp

--- basic_constructor&destructer.cpp
+++ basic_constructor&destructer.cpp
@@ -27,6 +27,51 @@ int main()
     car.name  = "renger";  // accessing the data members;
     car.cost  = 1500000;   
 
-    car.details(); // acessing the member functions
+    car.details(); // acessing the member functions 
+
+}
+
+// ======================================================================
+// ======================================================================
+//  more clarity and another example
+#include<iostream>
+#include<string>
+using namespace std;
+class toys{              //plan for one toy
+    public :
+    
+    string toy_name;  // data_members require data for toy like what details
+    int toy_no;
+    int toy_cost;
+ 
+    // parameterized_construtor 
+    toys(string ch,int x,int y){    // it is used to initilize the data when object created time
+        toy_name = ch;
+        toy_no = x;
+        toy_cost = y;
+    }
+    // member_functions used to do functions with data_members like displaying details updating them etc...
+    // lets say print or display them
+    void display()
+    {
+        cout  << toy_name << " " << toy_no << " " << toy_cost << endl;
+    }
+};
+int main ()
+{   
+       // now create an object using class_name like  ::> class_name object_name
+    //    toys toy1;
+    //    toys toy2;  this rises error it's error
+    //    toys toy3;
+       // initilize the data to oject using constructor
+       //i.e
+       // correct is
+       toys toy1("car",101,2000);
+       toys toy2("bike",100,1500);
+       toys toy3("barbie_doll",99,1200);
+       // displaying using member function 
+       toy1.display();
+       toy2.display();
+       toy3.display();
 
 }
